977d14a63520446ec6ec07c5b36910312ba8ce8b
Christopher Mayorga <christopher.mayorga2@gmail.com>
2021-07-12 14:43:34 -0500

    MVP push

--- Prepare.py
+++ Prepare.py
@@ -100,3 +100,61 @@ def train_validate_test(df, target):
     y_test = test[target]
     
     return train, validate, X_train, y_train, X_validate, y_validate, X_test, y_test
+
+def get_object_cols(df):
+    '''
+    This function takes in a dataframe and identifies the columns that are object types
+    and returns a list of those column names. 
+    '''
+    # create a mask of columns whether they are object type or not
+    mask = np.array(df.dtypes == "object")
+
+        
+    # get a list of the column names that are objects (from the mask)
+    object_cols = df.iloc[:, mask].columns.tolist()
+    
+    return object_cols
+
+def get_numeric_X_cols(X_train, object_cols):
+    '''
+    takes in a dataframe and list of object column names
+    and returns a list of all other columns names, the non-objects. 
+    '''
+    numeric_cols = [col for col in X_train.columns.values if col not in object_cols]
+    
+    return numeric_cols
+
+def min_max_scale(X_train, X_validate, X_test, numeric_cols):
+    '''
+    this function takes in 3 dataframes with the same columns, 
+    a list of numeric column names (because the scaler can only work with numeric columns),
+    and fits a min-max scaler to the first dataframe and transforms all
+    3 dataframes using that scaler. 
+    it returns 3 dataframes with the same column names and scaled values. 
+    '''
+    # create the scaler object and fit it to X_train (i.e. identify min and max)
+    # if copy = false, inplace row normalization happens and avoids a copy (if the input is already a numpy array).
+     
+    scaler = MinMaxScaler(copy=True).fit(X_train[numeric_cols])
+
+    #scale X_train, X_validate, X_test using the mins and maxes stored in the scaler derived from X_train. 
+    # 
+    X_train_scaled_array = scaler.transform(X_train[numeric_cols])
+    X_validate_scaled_array = scaler.transform(X_validate[numeric_cols])
+    X_test_scaled_array = scaler.transform(X_test[numeric_cols])
+
+    # convert arrays to dataframes
+    X_train_scaled = pd.DataFrame(X_train_scaled_array, 
+                                  columns=numeric_cols).\
+                                  set_index([X_train.index.values])
+
+    X_validate_scaled = pd.DataFrame(X_validate_scaled_array, 
+                                     columns=numeric_cols).\
+                                     set_index([X_validate.index.values])
+
+    X_test_scaled = pd.DataFrame(X_test_scaled_array, 
+                                 columns=numeric_cols).\
+                                 set_index([X_test.index.values])
+
+    
+    return X_train_scaled, X_validate_scaled, X_test_scaled
